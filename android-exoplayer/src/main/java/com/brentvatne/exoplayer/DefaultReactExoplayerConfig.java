84571d763b2fab0b1a00892aad7dd9188d375791
jan van der taelen <jan@pointbreak.co>
2022-06-22 10:59:53 +0200

    use TiMPPerformanceBitrateAdaptionPreset

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/DefaultReactExoplayerConfig.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/DefaultReactExoplayerConfig.java
@@ -3,7 +3,7 @@ package com.brentvatne.exoplayer;
 import android.content.Context;
 
 import com.brentvatne.exoplayer.bitrate.BitrateAdaptionPreset;
-import com.brentvatne.exoplayer.bitrate.DefaultBitrateAdaptionPreset;
+import com.brentvatne.exoplayer.bitrate.TiMPPerformanceBitrateAdaptionPreset;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultLoadErrorHandlingPolicy;
 import com.google.android.exoplayer2.upstream.LoadErrorHandlingPolicy;
@@ -15,7 +15,7 @@ public class DefaultReactExoplayerConfig implements ReactExoplayerConfig {
     private final DefaultBandwidthMeter bandwidthMeter;
 
     public DefaultReactExoplayerConfig(Context context) {
-        this.bitrateAdaptionPreset = new DefaultBitrateAdaptionPreset();
+        this.bitrateAdaptionPreset = new TiMPPerformanceBitrateAdaptionPreset();
         this.bandwidthMeter = new DefaultBandwidthMeter.Builder(context)
                 .setInitialBitrateEstimate(this.bitrateAdaptionPreset.maxInitialBitrate())
                 .setSlidingWindowMaxWeight(this.bitrateAdaptionPreset.bandwidthMeterMaxWeight())
